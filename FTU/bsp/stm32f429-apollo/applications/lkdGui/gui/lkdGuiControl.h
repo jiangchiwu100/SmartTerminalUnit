c4ce188ea9e5aa6915ac577258bd115785a012d6
guoweilkd <1274132245@qq.com>
2018-05-20 11:53:28 +0800

    添加文件lkdGui控件文件，后期会使用

---
+++ FTU/bsp/stm32f429-apollo/applications/lkdGui/gui/lkdGuiControl.h
@@ -0,0 +1,14 @@
+/**
+  * @file   lkdGuiControl.h
+  * @author  guoweilkd
+  * @version V1.1.0
+  * @date    2018/05/18
+  * @brief   Gui¿Ø¼þ
+  */
+	
+typedef struct{
+	uint8_t row;
+	uint8_t col;
+	uint8_t *pContent;
+	uint8_t 
+}lkdList;
