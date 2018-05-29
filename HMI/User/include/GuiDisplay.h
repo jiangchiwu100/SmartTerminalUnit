3bb89c966a38fa8e17cccc4ca1e98098dd45bddc
guoweilkd <1274132245@qq.com>
2018-05-29 16:06:10 +0800

    添加HMI修改.h文件

---
+++ HMI/User/include/GuiDisplay.h
@@ -0,0 +1,22 @@
+/**
+  * @file    GuiDisplay.h
+  * @author  guoweilkd
+  * @version V1.1.0
+  * @date    2018/05/28
+  * @brief   
+  */
+  
+#ifndef _GUIDISPLAY_H_
+#define _GUIDISPLAY_H_
+
+#include "stdint.h"
+
+#define USEPOWERONIMAGE 0
+#define VERSIONNUMBER "V001.001.180528"
+
+/* GUI∆Ù∂ØΩÁ√Ê */
+void GUIStartInterface(void);
+
+#endif /* END _GUIDISPLAY_H_ */
+
+/* END */
