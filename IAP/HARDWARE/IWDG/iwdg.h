05310c9529d38625a3ea8913873d952d837d7209
guoweilkd <1274132245@qq.com>
2018-06-03 08:22:48 +0800

    删除IAP,以后使用FTUBoot

--- IAP/HARDWARE/IWDG/iwdg.h
+++
@@ -1,8 +0,0 @@
-#ifndef __IWDG_H
-#define __IWDG_H
-#include "sys.h"
-
-
-void IWDG_Init(u8 prer,u16 rlr);//初始化IWDG，并使能IWDG
-void WDG_FEED(void);
-#endif
--- IAP/HARDWARE/LED/led.h
+++
@@ -1,7 +0,0 @@
-#ifndef _LED_H
-#define _LED_H
-#include "sys.h"
-
-void LED_Init(void);
-void LedRun(void);
-#endif
